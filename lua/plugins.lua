3fea015dca8a38887fdc16a2c6f2bb4c92ae55b0
Rafael Paz <rafael@collato.com>
2022-09-13 18:05:10 +0200

    snippet test

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -155,11 +155,51 @@ return require('packer').startup({
     use { 'zhou13/vim-easyescape' }
     use { 'kevinhwang91/rnvimr' }
     use { 'rgroli/other.nvim', config = "require('plugins.other')" }
-    use { 'abecodes/tabout.nvim', config = "require('plugins.tabout')" }
-
+    -- Lua
+    use {
+      'abecodes/tabout.nvim',
+      config = function()
+        require('tabout').setup {
+          tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+          backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+          act_as_tab = true, -- shift content if tab out is not possible
+          act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+          default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+          default_shift_tab = '<C-d>', -- reverse shift default action,
+          enable_backwards = true, -- well ...
+          completion = true, -- if the tabkey is used in a completion pum
+          tabouts = {
+            { open = "'", close = "'" },
+            { open = '"', close = '"' },
+            { open = '`', close = '`' },
+            { open = '(', close = ')' },
+            { open = '[', close = ']' },
+            { open = '{', close = '}' }
+          },
+          ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+          exclude = {} -- tabout will ignore these filetypes
+        }
+      end,
+      wants = { 'nvim-treesitter' }, -- or require if not used so far
+    }
     use { 'simrat39/symbols-outline.nvim', config = function()
       require('symbols-outline').setup()
     end }
+
+    use({
+      "Pocco81/auto-save.nvim",
+      config = function()
+        require("auto-save").setup {
+        }
+      end,
+    })
+
+    use "bkad/CamelCaseMotion"
+
+    use { "catppuccin/nvim", as = "catppuccin", config = function()
+      require("catppuccin").setup()
+      vim.g.catppuccin_flavour = "mocha"
+    end, run = ":CatppuccinCompile" }
 
     if packer_bootstrap then
       require('packer').sync()
